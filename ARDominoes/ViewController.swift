be53e96c1a712bdfbc9431f6fd684aed1f77cd13
iptkrisna <krisnamath9@gmail.com>
2019-07-09 18:26:31 +0700

    before physics-body

--- ARDominoes/ViewController.swift
+++ ARDominoes/ViewController.swift
@@ -13,6 +13,10 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var detectedPlanes: [String : SCNNode] = [:]
+    var dominoes: [SCNNode] = []
+    var previousDominoPosition: SCNVector3?
+    let dominoColors: [UIColor] = [.red, .blue, .green, .yellow, .orange, .cyan, .magenta, .purple]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +28,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(screenPanned))
+        sceneView.addGestureRecognizer(panGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +42,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -57,6 +66,87 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // 1
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        // 2
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.position = SCNVector3Make(planeAnchor.center.x,
+                                            planeAnchor.center.y,
+                                            planeAnchor.center.z)
+        // 3
+        planeNode.opacity = 0.3
+        // 4
+        planeNode.rotation = SCNVector4Make(1, 0, 0, -Float.pi / 2.0)
+        node.addChildNode(planeNode)
+        // 5
+        detectedPlanes[planeAnchor.identifier.uuidString] = planeNode
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        // 2
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.position = SCNVector3Make(planeAnchor.center.x,
+                                            planeAnchor.center.y,
+                                            planeAnchor.center.z)
+        
+        planeNode.opacity = 0.0
+        
+    }
+    
+    @objc func screenPanned(gesture: UIPanGestureRecognizer) {
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+        
+        let location = gesture.location(in: sceneView)
+        guard let hitTestResult = sceneView.hitTest(location, types: .existingPlane).first else { return }
+        // 1
+        guard let previousPosition = previousDominoPosition else {
+            self.previousDominoPosition = SCNVector3Make(hitTestResult.worldTransform.columns.3.x,
+                                                         hitTestResult.worldTransform.columns.3.y,
+                                                         hitTestResult.worldTransform.columns.3.z)
+            return
+        }
+        // 2
+        let currentPosition = SCNVector3Make(hitTestResult.worldTransform.columns.3.x,
+                                             hitTestResult.worldTransform.columns.3.y,
+                                             hitTestResult.worldTransform.columns.3.z)
+        // 3
+        let minimumDistanceBetweenDominoes: Float = 0.03
+        let distance = distanceBetween(point1: previousPosition, andPoint2: currentPosition)
+        if distance >= minimumDistanceBetweenDominoes {
+            let dominoGeometry = SCNBox(width: 0.007, height: 0.06, length: 0.03, chamferRadius: 0.0)
+            dominoGeometry.firstMaterial?.diffuse.contents = dominoColors.randomElement()
+            let dominoNode = SCNNode(geometry: dominoGeometry)
+            dominoNode.position = SCNVector3Make(currentPosition.x,
+                                                 currentPosition.y + 0.03,
+                                                 currentPosition.z)
+            // 1
+            var currentAngle: Float = pointPairToBearingDegrees(startingPoint: CGPoint(x: CGFloat(currentPosition.x), y: CGFloat(currentPosition.z)), secondPoint: CGPoint(x: CGFloat(previousPosition.x), y: CGFloat(previousPosition.z)))
+            // 2
+            currentAngle *= .pi / 180
+            // 3
+            dominoNode.rotation = SCNVector4Make(0, 1, 0, -currentAngle)
+            sceneView.scene.rootNode.addChildNode(dominoNode)
+            dominoes.append(dominoNode)
+            // 4
+            self.previousDominoPosition = currentPosition
+        }
+    }
+    
+    func distanceBetween(point1: SCNVector3, andPoint2 point2: SCNVector3) -> Float {
+        return hypotf(Float(point1.x - point2.x), Float(point1.z - point2.z))
+    }
+    
+    func pointPairToBearingDegrees(startingPoint: CGPoint, secondPoint endingPoint: CGPoint) -> Float{
+        let originPoint: CGPoint = CGPoint(x: startingPoint.x - endingPoint.x, y: startingPoint.y - endingPoint.y)
+        let bearingRadians = atan2f(Float(originPoint.y), Float(originPoint.x))
+        let bearingDegrees = bearingRadians * (180.0 / Float.pi)
+        return bearingDegrees
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
